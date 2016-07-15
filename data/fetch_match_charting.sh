df5820b4d04abb0ec04cc65c47e25f2426f7298b
Martin Ingram <martin@stratagem.co>
2016-07-15 13:16:46 +1000

    Added small script to fetch MatchCharting

---
+++ data/fetch_match_charting.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Clones Sackmann's Match Charting repository
+git clone https://github.com/JeffSackmann/tennis_MatchChartingProject.git
--- data/sackmann/fetch_sackmann.sh
+++ data/sackmann/fetch_sackmann.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# Clones Sackmann's tennis repository
+# Clones Sackmann's match results repository
 git clone https://github.com/JeffSackmann/tennis_atp.git
