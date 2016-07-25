81e490249e33d3a610f91265d700e9b80cf8e631
martiningram <martin@stratagem.co>
2016-07-25 17:45:07 +1000

    Added annotated scatter utility

--- tdata/utils.py
+++ tdata/utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import time
 from functools import wraps
 
@@ -43,3 +45,26 @@ def retry(ExceptionToCheck, tries=4, delay=3, backoff=2, logger=None):
         return f_retry  # true decorator
 
     return deco_retry
+
+
+def plot_annotated_scatter(labels, xs, ys, plot_line=False):
+
+    plt.subplots_adjust(bottom=0.1)
+
+    plt.scatter(xs, ys, marker='o')
+
+    for label, x, y in zip(labels, xs, ys):
+        plt.annotate(
+            label,
+            xy=(x, y), xytext=(-10, 10),
+            textcoords='offset points', ha='right', va='bottom',
+            bbox = dict(boxstyle='round,pad=0.3', fc='yellow',
+                        alpha=0.5),
+            arrowprops = dict(arrowstyle='->', connectionstyle='arc3,rad=0'),
+            fontsize = 8)
+
+    if (plot_line):
+        coeff = np.polyfit(xs, ys, 1)
+        plt.plot(xs, np.polyval(coeff, xs))
+
+    return plt
